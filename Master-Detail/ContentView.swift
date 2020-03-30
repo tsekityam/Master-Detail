0e6a1b8e89d2434fea77e0194707ff6b7b904e80
Tse Kit Yam <me@kytse.com>
2020-03-30 13:55:15 +0800

    Add sample code

--- Master-Detail/ContentView.swift
+++ Master-Detail/ContentView.swift
@@ -8,74 +8,51 @@
 
 import SwiftUI
 
-private let dateFormatter: DateFormatter = {
-    let dateFormatter = DateFormatter()
-    dateFormatter.dateStyle = .medium
-    dateFormatter.timeStyle = .medium
-    return dateFormatter
-}()
-
 struct ContentView: View {
-    @Environment(\.managedObjectContext)
-    var viewContext   
- 
+    let tabOneMax = 100
+    let tabTwoMax = 100
+
     var body: some View {
         NavigationView {
-            MasterView()
-                .navigationBarTitle(Text("Master"))
-                .navigationBarItems(
-                    leading: EditButton(),
-                    trailing: Button(
-                        action: {
-                            withAnimation { Event.create(in: self.viewContext) }
-                        }
-                    ) { 
-                        Image(systemName: "plus")
+            TabView {
+                MasterView(max: tabOneMax)
+                    .tabItem {
+                        Image(systemName: "list.dash")
+                        Text("\(tabOneMax)")
                     }
-                )
-            Text("Detail view content goes here")
-                .navigationBarTitle(Text("Detail"))
+                MasterView(max: tabTwoMax)
+                    .tabItem {
+                        Image(systemName: "list.dash")
+                        Text("\(tabTwoMax)")
+                    }
+            }.navigationBarTitle(Text("Master"))
+
+            DetailView()
         }.navigationViewStyle(DoubleColumnNavigationViewStyle())
     }
 }
 
 struct MasterView: View {
-    @FetchRequest(
-        sortDescriptors: [NSSortDescriptor(keyPath: \Event.timestamp, ascending: true)], 
-        animation: .default)
-    var events: FetchedResults<Event>
-
-    @Environment(\.managedObjectContext)
-    var viewContext
+    var max: Int
 
     var body: some View {
         List {
-            ForEach(events, id: \.self) { event in
-                NavigationLink(
-                    destination: DetailView(event: event)
-                ) {
-                    Text("\(event.timestamp!, formatter: dateFormatter)")
-                }
-            }.onDelete { indices in
-                self.events.delete(at: indices, from: self.viewContext)
+            ForEach(0..<max, id: \.self) { number in
+                Text("\(number)")
             }
         }
     }
 }
 
 struct DetailView: View {
-    @ObservedObject var event: Event
-
     var body: some View {
-        Text("\(event.timestamp!, formatter: dateFormatter)")
-            .navigationBarTitle(Text("Detail"))
+        Text("Detail view content goes here")
     }
 }
 
 
 struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
-        let context = (UIApplication.shared.delegate as! AppDelegate).persistentContainer.viewContext
-        return ContentView().environment(\.managedObjectContext, context)
+        ContentView()
     }
 }
